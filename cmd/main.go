711624ad4ee4365d8ab9559df72209fc40c2d5e0
espiridonova <e.s.spiridonova00@mail.ru>
2025-06-23 13:35:18 +0300

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -28,6 +28,7 @@ func main() {
 	http.HandleFunc("/create", handler.CreateHandler)
 	http.HandleFunc("/update", handler.UpdateHandler)
 	http.HandleFunc("/delete", handler.DeleteHandler)
+
 	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
